e850cc8cc09f42693f844ecd0a3535fc6630f339
LABriset <labsosuny@gmail.com>
2021-05-03 15:46:15 +0700

    adding

---
+++ a.R
@@ -0,0 +1,9 @@
+
+blogdown::new_site(theme= "yihui/hugo-lithium")
+blogdown::new_site(theme= "h-enk/doks-child-theme.git")
+
+blogdown::new_post()
+
+blogdown::build_site()
+blogdown::serve_site()
+blogdown::new_post(title = "Paper Tawang")
