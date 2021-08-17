2a317ec58519525ea007b3484c99b42eaa56f5eb
puja <labsosuny@gmail.com>
2021-08-17 21:38:32 +0700

    add git

--- script.R
+++ script.R
@@ -8,3 +8,12 @@ blogdown::stop_server()
 labsosuny@gmail.com
 labsos123
 
+#Selanjutnya, masukkan username GitHub Anda menggunakan perintah di bawah ini. Lalu tekan ENTER jika sudah benar.
+
+#$ git config --global user.name "UsernameAnda"
+#Kemudian masukkan email yang terdaftar di GitHub Anda menggunakan perintah di bawah  ini. Lalu tekan ENTER jika sudah benar.
+
+#$ git config --global user.email IsiDenganEmailAnda@gmail.com
+#Selanjutnya untuk memastikan proses login Anda berhasil, masukkan perintah berikut.
+
+#$ git config --list
